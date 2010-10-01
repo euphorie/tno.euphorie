45936c1d2a938830e0e0460fa0edec777e8b0264
Wichert Akkerman <wichert@wiggy.net>
2010-10-01 10:37:32 +0000

    Add local interfaces for more client customisation.

--- buildout/src/tno.euphorie/tno/euphorie/interfaces.py
+++ buildout/src/tno.euphorie/tno/euphorie/interfaces.py
@@ -1,5 +1,9 @@
 from zope.interface import Interface
 from euphorie.client.interfaces import IClientSkinLayer
+from euphorie.client.interfaces import IIdentificationPhaseSkinLayer
+from euphorie.client.interfaces import IEvaluationPhaseSkinLayer
+from euphorie.client.interfaces import IActionPlanPhaseSkinLayer
+from euphorie.client.interfaces import IReportPhaseSkinLayer
 
 class IProductLayer(Interface):
     """Marker interface for requests indicating the tno.euphorie
@@ -9,3 +13,20 @@ class IProductLayer(Interface):
 
 class ITnoClientSkinLayer(IClientSkinLayer):
     """Marker interface for the TNO client skin."""
+
+
+class ITnoIdentificationPhaseSkinLayer(IIdentificationPhaseSkinLayer):
+    """Marker interface for the identification phase in a tno.euphorie site."""
+
+
+class ITnoEvaluationPhaseSkinLayer(IEvaluationPhaseSkinLayer):
+    """Marker interface for the evaluation phase in a tno.euphorie site."""
+
+
+class ITnoActionPlanPhaseSkinLayer(IActionPlanPhaseSkinLayer):
+    """Marker interface for the action plan phase in a tno.euphorie site."""
+
+
+class ITnoReportPhaseSkinLayer(IReportPhaseSkinLayer):
+    """Marker interface for the report phase in a tno.euphorie site."""
+
---
+++ buildout/src/tno.euphorie/tno/euphorie/survey.py
@@ -0,0 +1,15 @@
+from euphorie.client.survey import SurveyPublishTraverser
+from tno.euphorie.interfaces import ITnoIdentificationPhaseSkinLayer
+from tno.euphorie.interfaces import ITnoEvaluationPhaseSkinLayer
+from tno.euphorie.interfaces import ITnoActionPlanPhaseSkinLayer
+from tno.euphorie.interfaces import ITnoReportPhaseSkinLayer
+
+
+class TnoSurveyPublishTraverser(SurveyPublishTraverser):
+    phases = {
+            "identification": ITnoIdentificationPhaseSkinLayer,
+            "evaluation": ITnoEvaluationPhaseSkinLayer,
+            "actionplan": ITnoActionPlanPhaseSkinLayer,
+            "report": ITnoReportPhaseSkinLayer,
+            }
+

c07f8c958a123d73dd15ba4023f390f39949c0aa
Wichert Akkerman <wichert@wiggy.net>
2010-10-01 10:44:51 +0000

    Copy TNO specific report things here as well.

--- buildout/src/tno.euphorie/tno/euphorie/survey.py
+++ buildout/src/tno.euphorie/tno/euphorie/survey.py
@@ -1,9 +1,12 @@
+from five import grok
 from euphorie.client.survey import SurveyPublishTraverser
+from euphorie.client.survey import ActionPlanReportView
 from tno.euphorie.interfaces import ITnoIdentificationPhaseSkinLayer
 from tno.euphorie.interfaces import ITnoEvaluationPhaseSkinLayer
 from tno.euphorie.interfaces import ITnoActionPlanPhaseSkinLayer
 from tno.euphorie.interfaces import ITnoReportPhaseSkinLayer
 
+grok.templatedir("templates")
 
 class TnoSurveyPublishTraverser(SurveyPublishTraverser):
     phases = {
@@ -12,4 +15,10 @@ class TnoSurveyPublishTraverser(SurveyPublishTraverser):
             "actionplan": ITnoActionPlanPhaseSkinLayer,
             "report": ITnoReportPhaseSkinLayer,
             }
+
+
+class TnoActionPlanReportView(ActionPlanReportView):
+    grok.layer(ITnoReportPhaseSkinLayer)
+    grok.name("view")
+    grok.template("report_actionplan")
 

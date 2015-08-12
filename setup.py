89025caa0cdbd82061cee1ec592e59c4f4abc93e
Wichert Akkerman <wichert@wiggy.net>
2015-08-12 12:19:20 +0200

    Fix GS upgrade errors.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '5.0b1'
+version = '5.0b2'
 
 setup(name="tno.euphorie",
       version=version,
--- src/tno/euphorie/upgrade/v2.py
+++ src/tno/euphorie/upgrade/v2.py
@@ -1,13 +1,28 @@
 import logging
 import transaction
+from sqlalchemy.exc import NoSuchTableError
+from sqlalchemy.schema import MetaData
+from sqlalchemy.schema import Table
 from z3c.saconfig import Session
 from zope.sqlalchemy import datamanager
 from euphorie.deployment.upgrade.utils import TableExists
-from euphorie.deployment.upgrade.utils import ColumnExists
 from tno.euphorie.model import OdLink
 
 
 log = logging.getLogger(__name__)
+
+
+# Modified version of euphorie.deployment.upgarde.utils.ColumnExists which
+# fixes the last argument to reflecttable.
+def column_exists(session, table, column):
+    connection = session.bind
+    metadata = MetaData(connection)
+    table = Table(table, metadata)
+    try:
+        connection.dialect.reflecttable(connection, table, None, [])
+    except NoSuchTableError:
+        return False
+    return column in table.c
 
 
 def add_od_table(context):
@@ -19,7 +34,7 @@ def add_od_table(context):
 
 def add_od_version_column(context):
     session = Session()
-    if ColumnExists(session, 'od_link', 'version'):
+    if column_exists(session, 'od_link', 'version'):
         return
     transaction.egt().commit()
     session.execute('ALTER TABLE od_link ADD COLUMN version INT DEFAULT 0 NOT NULL')

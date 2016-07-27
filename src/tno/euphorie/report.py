75a3cbac50c1a244010513d67d9dfeac4f878c94
Wolfgang Thomas <thomas@syslab.com>
2016-07-27 14:46:28 +0200

    We now have a different report landing page

--- src/tno/euphorie/report.py
+++ src/tno/euphorie/report.py
@@ -8,7 +8,6 @@ from five import grok
 from zope.i18n import translate
 from plonetheme.nuplone.utils import formatDate
 from euphorie.client import model
-from euphorie.client.report import ActionPlanReportView
 from euphorie.client.report import ActionPlanReportDownload
 from euphorie.client.report import createSection
 from euphorie.client import MessageFactory as eu_
@@ -28,28 +27,6 @@ def formatAddress(address, postal, city):
     if bits:
         output.append(u" ".join(bits))
     return u"".join(output) if output else None
-
-
-
-class TnoActionPlanReportView(ActionPlanReportView):
-    grok.layer(ITnoReportPhaseSkinLayer)
-    grok.name("view")
-    grok.template("report_actionplan")
-
-    def getNodes(self):
-        query = Session.query(model.SurveyTreeItem)\
-                .filter(model.SurveyTreeItem.session == self.session)\
-                .filter(sql.not_(model.SKIPPED_PARENTS))\
-                .filter(sql.or_(model.MODULE_WITH_RISK_OR_TOP5_FILTER,
-                                model.RISK_PRESENT_OR_TOP5_FILTER))\
-                .order_by(model.SurveyTreeItem.path)
-        return  query.all()
-
-    def update(self):
-        super(TnoActionPlanReportView, self).update()
-        if self.session.dutch_company is None:
-            self.session.dutch_company=DutchCompany()
-
 
 
 class TnoActionPlanReportDownload(ActionPlanReportDownload):

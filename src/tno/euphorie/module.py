10e1c5bc669695b1195ec95f41372760fd2c593c
Wolfgang Thomas <thomas@syslab.com>
2018-12-12 16:11:42 +0100

    Get rid of RISK_OR_MODULE_WITH_DESCRIPTION_FILTER again. I don't understand why it got introduced 6 years ago. In today's version, it leads to inconsistent behaviour in the navigation of the action plan

--- src/tno/euphorie/module.py
+++ src/tno/euphorie/module.py
@@ -13,7 +13,6 @@ class TnoActionPlanView(ActionPlanView):
     grok.layer(ITnoActionPlanPhaseSkinLayer)
     grok.name('index_html')
 
-    question_filter = sql.and_(
-            model.RISK_OR_MODULE_WITH_DESCRIPTION_FILTER,
-            sql.or_(model.MODULE_WITH_RISK_FILTER,
-                    model.RISK_PRESENT_FILTER))
+    question_filter = sql.or_(
+        model.MODULE_WITH_RISK_FILTER,
+        model.RISK_PRESENT_FILTER)

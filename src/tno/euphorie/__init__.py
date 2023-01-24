f9bfed75201dd2e537d654cebffbb5764586ebd6
ale-rt <alessandro.pisa@gmail.com>
2023-01-24 14:30:06 +0100

    Hide the organisation tab
    Patch the WebHelpers class to hide the organisation tab
    
    See https://github.com/syslabcom/scrum/issues/519

--- src/tno/euphorie/__init__.py
+++ src/tno/euphorie/__init__.py
@@ -1,7 +1,12 @@
 from zope.i18nmessageid import MessageFactory as mf
+from euphorie.client.browser.webhelpers import WebHelpers
 
+# Patch the WebHelpers class to hide the organisation tab
+# See https://github.com/syslabcom/scrum/issues/519
+WebHelpers.hide_organisation_tab = True
 
 MessageFactory = mf("tno.euphorie")
 del mf
+
 
 __all__ = ["MessageFactory"]

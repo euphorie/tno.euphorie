0c3e04154a7973e4d040e653703d252eba8cd057
ale-rt <alessandro.pisa@gmail.com>
2024-02-06 11:07:13 +0100

    Improve the way we set the client skin layer
    Requires https://github.com/euphorie/Euphorie/pull/695

--- src/tno/euphorie/client/client.py
+++ src/tno/euphorie/client/client.py
@@ -1,32 +1,16 @@
+from euphorie.client.client import ClientPublishTraverser as BaseClientPublishTraverser
 from euphorie.client.client import IClient
 from tno.euphorie.interfaces import IProductLayer
 from tno.euphorie.interfaces import ITnoClientSkinLayer
-from zope.component import adapts
-from zope.interface import directlyProvidedBy
-from zope.interface import directlyProvides
-from zope.publisher.interfaces.browser import IBrowserSkinType
-from ZPublisher.BaseRequest import DefaultPublishTraverse
+from zope.component import adapter
 
 
-class ClientPublishTraverser(DefaultPublishTraverse):
+@adapter(IClient, IProductLayer)
+class ClientPublishTraverser(BaseClientPublishTraverser):
     """Publish traverser to setup the skin layer.
 
     This traverser marks the request with ITnoClientSkinLayer when the
     client is traversed and the tno.euphorie product is installed.
     """
 
-    adapts(IClient, IProductLayer)
-
-    def publishTraverse(self, request, name):
-        from euphorie.client.utils import setRequest
-
-        setRequest(request)
-        request.client = self.context
-
-        ifaces = [
-            iface
-            for iface in directlyProvidedBy(request)
-            if not IBrowserSkinType.providedBy(iface)
-        ]
-        directlyProvides(request, ITnoClientSkinLayer, ifaces)
-        return super(ClientPublishTraverser, self).publishTraverse(request, name)
+    skin_layer = ITnoClientSkinLayer
